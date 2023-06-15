508382297a66ff18e7cc2596d2fcacda41f9a9e0
Augustelli <a.mancuso@alumno.um.edu.ar>
2023-06-15 19:01:52 -0300

    VERSIÓN FUNCIONAL

--- TP5v2/analizador.py
+++ TP5v2/analizador.py
@@ -1,81 +1,125 @@
-'''ESTE NO FUNCAAAAAAAAAAAA'''
+import re
+import pandas as pd
 
 
+def last_conection(data_filter):
+    correcto = True
+    seguir = True
+    lista_columnas = []
+    opciones_columnas = {
+        '1': 'ID',
+        '2': 'ID_Sesion',
+        '3': 'ID_Conexión_unico',
+        '4': 'Usuario',
+        '5': 'IP_NAS_AP',
+        '6': 'Inicio_de_Conexión_Dia',
+        '7': 'Inicio_de_Conexión_Hora',
+        '8': 'FIN_de_Conexión_Dia',
+        '9': 'FIN_de_Conexión_Hora',
+        '10': 'Session_Time',
+        '11': 'MAC_AP',
+        '12': 'MAC_Cliente',
+    }
+    date_start = input('Ingrese la fecha de inicio de la busqueda (YYYY-MM-DD): ')
+    date_end = input('Ingrese la fecha de fin de la busqueda (YYYY-MM-DD): ')
 
-# import pandas as pd
-# import re
-# from multiprocessing import Pool
-# import tabulate
+    patron_start = re.compile(r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$')
+    patron_end = re.compile(r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$')
 
-# # Expresiones regulares para cada columna
-# expresiones_regulares = {
-#     'ID': r'^\d{6,7}$',
-#     'ID_Sesion': r'^[A-F0-9]{8}-[A-F0-9]{8}$',
-#     'ID_Conexión_unico': r'^[a-f0-9]{16}$',
-#     'Usuario': r'^[a-zA-Z-]{3,25}$',
-#     'IP_NAS_AP': r'^192\.168\.247\.[0-9]{2}$',
-#     'Inicio_de_Conexión_Dia': r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$',
-#     'Inicio_de_Conexión_Hora': r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$',
-#     'FIN_de_Conexión_Dia': r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$',
-#     'FIN_de_Conexión_Hora': r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$',
-#     'Session_Time': r'^\d+(\.\d+)?$'
-#     # 'MAC_AP':  r'^([0-9A-Fa-f]{2}-){5}[0-9A-Fa-f]{2}:HCDD$',
-#     # 'MAC_Cliente': r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'
-# }
+    if patron_start.match(date_start) and patron_end.match(date_end):
+        print('Fecha de inicio y fin correctas')
+        
+           
+        while correcto is True:
+            rta2 = input(' Desea ver:\n\
+                        1. los usuarios que han iniciado y finalizado su conexión en el intervalo de tiempo \n\
+                        2.  Su conexión haya terminado en el intervalo de tiempo\n\
+                        Respuesta : ')
+            if rta2 == '1':
+                correcto = False
+                data_filter = data_filter[(data_filter['Inicio_de_Conexión_Dia'] >= date_start) & (data_filter['FIN_de_Conexión_Dia'] <= date_end)]
+            elif rta2 == '2':
+                correcto = False
+                data_filter = data_filter[(data_filter['FIN_de_Conexión_Dia'] >= date_start) & (data_filter['FIN_de_Conexión_Dia'] <= date_end)]
+            else:
+                print(f'Opción {rta2} incorrecta, intente denuevo. ')
 
-# def table():
-#     table=[[f'{key}',f'{value}'] for key,value in expresiones_regulares.items()]
-#     headers=['Columna','Expresión Regular']
-#     table_final=tabulate(table,headers,tablefmt='fancy_grid')
-#     print(table_final)
-
-
-
-# # Cargar el archivo CSV como un DataFrame en Pandas utilizando multiprocessing
-# def cargar_csv(path):
-#     data=pd.read_csv(path,low_memory=False,usecols=expresiones_regulares.keys())
-    
-#     return data
-
-
-
-
-# # Ruta y nombre de tu archivo CSV
-# csv_file = './TP5v2/file/export-2019-to-now-v4.csv'
-
-# # Cargar el archivo CSV utilizando multiprocessing
-# with Pool() as pool:
-#     data_frame = pd.concat(pool.map(cargar_csv, [csv_file]))
-
-# # Crear DataFrames para los registros válidos e inválidos
-# registros_validos = pd.DataFrame(columns=data_frame.columns)
-# registros_invalidos = pd.DataFrame(columns=data_frame.columns)
-
-
-# # Función para validar un registro
-# def validar_registro(registro):
-#     valido = True
-#     for columna, valor in registro.items():
-#         expresion_regular = expresiones_regulares.get(columna)
-#         if expresion_regular:
-#             if not re.match(expresion_regular, str(valor)):
-#                 valido = False
-#                 break
-#     return valido
+        while seguir:
+            columnas_a_aniadir = input('''Seleccione los campos para la tabla:\n
+                    1: ID
+                    2: ID_Sesion
+                    3: ID_Conexión_unico
+                    4: Usuario
+                    5: IP_NAS_AP
+                    6: Inicio_de_Conexión_Dia
+                    7: Inicio_de_Conexión_Hora
+                    8: FIN_de_Conexión_Dia
+                    9: FIN_de_Conexión_Hora
+                    10: Session_Time
+                    11: MAC_AP
+                    12: MAC_Cliente
+                    E: Salir ''')
+            if columnas_a_aniadir in opciones_columnas.keys():
+                lista_columnas.append(opciones_columnas[columnas_a_aniadir])
+            else:
+                seguir = False
+        data_filter_finally = data_filter[lista_columnas]
+        print('\n')
+        print(data_filter_finally)
+        print('\n')
+        export(data_filter_finally)
+    else:
+        print('Fecha de inicio y/o fin incorrectas')
+        last_conection(data_filter)
 
 
-# # Validar cada registro del DataFrame utilizando multiprocessing
-# with Pool() as pool:
-#     resultados = pool.map(validar_registro, data_frame.to_dict('records'))
+def export(data_filter_finally):
+    rta = input('Desea exportar los datos? (Y/N):').lower()
+    if rta == 'y':
+        rta1 = input('Desea exportar los datos en formato CSV o Excel? (CSV/Excel):').lower()
+        if rta1 == 'csv':
+            ruta_csv = './TP5v2/export/last_conection.csv'
+            data_filter_finally.to_csv(ruta_csv, index=False)
+        if rta1 == 'excel':
+            ruta_excel = './TP5v2/export/last_conection.xlsx'
+            data_filter_finally.to_excel(ruta_excel, index=False)
+    else:
+        print('Gracias por usar el programa')
 
-# # Dividir los registros válidos e inválidos
-# for index, registro in enumerate(data_frame.iterrows()):
-#     if resultados[index]:
-#         registros_validos = pd.concat([registros_validos, registro[1]])
-#     else:
-#         registros_invalidos = pd.concat([registros_invalidos, registro[1]])
 
-# # Mostrar los primeros 100 registros válidos
-# primeros_registros_validos = registros_validos.head(100)
-# print(primeros_registros_validos)
-# ###
+expresiones_regulares = {
+    'ID': r'^\d{6,7}$',
+    'ID_Sesion': r'^[A-F0-9]{8}-[A-F0-9]{8}$',
+    'ID_Conexión_unico': r'^[a-f0-9]{16}$',
+    'Usuario': r'^[a-zA-Z-]{3,25}$',
+    'IP_NAS_AP': r'^192\.168\.247\.[0-9]{2}$',
+    'Inicio_de_Conexión_Dia': r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$',
+    'Inicio_de_Conexión_Hora': r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$',
+    'FIN_de_Conexión_Dia': r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$',
+    'FIN_de_Conexión_Hora': r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$',
+    'Session_Time': r'^\d+(\.\d+)?$',
+    'MAC_AP':  r'^([0-9A-Fa-f]{2}-){5}[0-9A-Fa-f]{2}:HCDD$',
+    'MAC_Cliente': r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'
+    # Razon de terminacion de sesioncon
+}
+
+csv_file = './TP5v2/file/export-2019-to-now-v4.csv'
+data = pd.read_csv(csv_file, low_memory=False, usecols=expresiones_regulares.keys())
+
+data_filter = data
+for columna, patron in expresiones_regulares.items():
+    regex = re.compile(patron)
+    data_filter = data_filter[data_filter[columna].astype(str).str.match(regex)]
+# print(data_filter
+continuar = True
+while continuar is True:
+    last_conection(data_filter)
+    rta3 = input('¿Desea volver a consultar? (Y/N)').lower()
+    if rta3 == 'y':
+        pass
+    elif rta3 == 'n':
+        continuar = False
+    else:
+        print('Opción incorrecta, volver a ingresar')
+
+print('No vimo conshesumare')
--- TP5v2/analizadorv2.py
+++ TP5v2/analizadorv2.py
@@ -1,15 +1,105 @@
 import re
 import pandas as pd
 
+
+def last_conection(data_filter):
+    correcto = True
+    seguir = True
+    lista_columnas = []
+    opciones_columnas = {
+        '1': 'ID',
+        '2': 'ID_Sesion',
+        '3': 'ID_Conexión_unico',
+        '4': 'Usuario',
+        '5': 'IP_NAS_AP',
+        '6': 'Inicio_de_Conexión_Dia',
+        '7': 'Inicio_de_Conexión_Hora',
+        '8': 'FIN_de_Conexión_Dia',
+        '9': 'FIN_de_Conexión_Hora',
+        '10': 'Session_Time',
+        '11': 'MAC_AP',
+        '12': 'MAC_Cliente',
+    }
+    date_start = input('Ingrese la fecha de inicio de la busqueda (YYYY-MM-DD): ')
+    date_end = input('Ingrese la fecha de fin de la busqueda (YYYY-MM-DD): ')
+    patron_start = re.compile(r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$')
+    patron_end = re.compile(r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$')
+
+    if (patron_start.match(date_start) and patron_end.match(date_end)) and (date_start <= date_end):
+        print('Fecha de inicio y fin correctas')
+        while correcto is True:
+            rta2 = input(' Desea ver:\n\
+                        1. los usuarios que han iniciado y finalizado su conexión en el intervalo de tiempo \n\
+                        2.  Su conexión haya terminado en el intervalo de tiempo\n\
+                        Respuesta : ')
+            if rta2 == '1':
+                correcto = False
+                data_filter = data_filter[(data_filter['Inicio_de_Conexión_Dia'] >= date_start) & (data_filter['FIN_de_Conexión_Dia'] <= date_end)]
+            elif rta2 == '2':
+                correcto = False
+                data_filter = data_filter[(data_filter['FIN_de_Conexión_Dia'] >= date_start) & (data_filter['FIN_de_Conexión_Dia'] <= date_end)]
+            else:
+                print(f'Opción {rta2} incorrecta, intente denuevo. ')
+
+        while seguir:
+            columnas_a_aniadir = input('''Seleccione los campos para la tabla:\n
+                    1: ID
+                    2: ID_Sesion
+                    3: ID_Conexión_unico
+                    4: Usuario
+                    5: IP_NAS_AP
+                    6: Inicio_de_Conexión_Dia
+                    7: Inicio_de_Conexión_Hora
+                    8: FIN_de_Conexión_Dia
+                    9: FIN_de_Conexión_Hora
+                    10: Session_Time
+                    11: MAC_AP
+                    12: MAC_Cliente
+                    E: Salir\nRespuesta:  ''')
+            if columnas_a_aniadir in opciones_columnas.keys():
+                lista_columnas.append(opciones_columnas[columnas_a_aniadir])
+            else:
+                seguir = False
+        data_filter_finally = (data_filter[lista_columnas])
+        print('\n')
+        print(data_filter_finally)
+        print('\n')
+        export(data_filter_finally)
+    else:
+        print('Fecha de inicio y/o fin incorrectas, volver a intentar')
+        last_conection(data_filter)
+
+
+def export(data_filter_finally):
+    rta = input('Desea exportar los datos? (Y/N):').lower()
+    if rta == 'y':
+        rta1 = input('Desea exportar los datos en formato CSV o Excel? (CSV/Excel):').lower()
+        if rta1 == 'csv':
+            nombre_archivo = input('Nombre del archvo: ')
+            ruta_csv = f'./TP5v2/export/{nombre_archivo}.csv'
+            data_filter_finally.to_csv(ruta_csv, index=False)
+        if rta1 == 'excel':
+            nombre_archivo = input('Nombre del archvo: ')
+            ruta_excel = f'./TP5v2/export/{nombre_archivo}.xlsx'
+            data_filter_finally.to_excel(ruta_excel, index=False)
+    else:
+        print('Gracias por usar el programa')
+
+
 expresiones_regulares = {
     'ID': r'^\d{6,7}$',
     'ID_Sesion': r'^[A-F0-9]{8}-[A-F0-9]{8}$',
+    'ID_Conexión_unico': r'^[a-f0-9]{16}$',
     'Usuario': r'^[a-zA-Z-]{3,25}$',
+    'IP_NAS_AP': r'^192\.168\.247\.[0-9]{2}$',
     'Inicio_de_Conexión_Dia': r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$',
     'Inicio_de_Conexión_Hora': r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$',
     'FIN_de_Conexión_Dia': r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$',
     'FIN_de_Conexión_Hora': r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$',
-    'Session_Time': r'^\d+(\.\d+)?$'
+    'Session_Time': r'^\d+(\.\d+)?$',
+    'MAC_AP':  r'^([0-9A-Fa-f]{2}-){5}[0-9A-Fa-f]{2}:HCDD$',
+    'MAC_Cliente': r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'
+    # Razon de terminacion de sesioncon
 }
 
 csv_file = './TP5v2/file/export-2019-to-now-v4.csv'
@@ -19,128 +109,15 @@ data_filter = data
 for columna, patron in expresiones_regulares.items():
     regex = re.compile(patron)
     data_filter = data_filter[data_filter[columna].astype(str).str.match(regex)]
-
-# print(data_filter)
-
-def last_conection(data_filter):
-    # try:
-        # date_start='2019-01-01'
-        # date_end='2019-05-02'
-
-
-        date_start=input('Ingrese la fecha de inicio de la busqueda (YYYY-MM-DD): ')
-        date_end=input('Ingrese la fecha de fin de la busqueda (YYYY-MM-DD): ')
-
-
-
-        patron_start=re.compile(r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$')
-        patron_end=re.compile(r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$')
-
-        if patron_start.match(date_start) and patron_end.match(date_end):
-            print('Fecha de inicio y fin correctas')
-
-        # data_filter[(data_filter['Inicio_de_Conexión_Dia']>= date_start) &(data_filter['FIN_de_Conexión_Dia'] >= date_start) & (data_filter['FIN_de_Conexión_Dia'] <= date_end)]
-        # data_filter=data_filter[(data_filter['Inicio_de_Conexión_Dia']>= date_start)  & (data_filter['FIN_de_Conexión_Dia'] <= date_end)]
-            data_filter=data_filter[(data_filter['FIN_de_Conexión_Dia'] >= date_start) & (data_filter['FIN_de_Conexión_Dia'] <= date_end)]
-
-            data_filter_finally=(data_filter[['ID','ID_Sesion','Usuario','FIN_de_Conexión_Dia']])
-            print(data_filter_finally)
-            export(data_filter_finally)
-        else:
-            print('Fecha de inicio y/o fin incorrectas')
-            last_conection(data_filter)
-
-
-
-   
-
-    # except Exception as e:
-    #     # Bloque de código para manejar cualquier otra excepción no capturada explícitamente
-    #     print("Error:", str(e))
-
-def export(data_filter_finally):
-    rta=input('Desea exportar los datos? (Y/N):')
-    if rta=='Y' or rta=='y':
-            rta1=input('Desea exportar los datos en formato CSV o Excel? (CSV/Excel):')
-            if rta1=='CSV' or rta1=='csv':
-         
-                ruta_csv='./TP5v2/export/last_conection.csv'
-                data_filter_finally.to_csv(ruta_csv, index=False)
-            if rta1=='Excel' or rta1=='excel':
-
-                ruta_excel='./TP5v2/export/last_conection.xlsx'
-                data_filter_finally.to_excel(ruta_excel, index=False)
+# print(data_filter
+continuar = True
+while continuar is True:
+    last_conection(data_filter)
+    rta3 = input('¿Desea volver a consultar? (Y/N):\nRespuesta: ').lower()
+    if rta3 == 'y':
+        pass
+    elif rta3 == 'n':
+        continuar = False
     else:
-        print('Gracias por usar el programa')
-
-last_conection(data_filter)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import re 
-# import pandas as pd
-# import tabulate
-
-# # Expresiones regulares para cada columna
-# expresiones_regulares = {
-#     'ID': r'^\d{6,7}$',
-#     'ID_Sesion': r'^[A-F0-9]{8}-[A-F0-9]{8}$',
-#     # 'ID_Conexión_unico': r'^[a-f0-9]{16}$',
-#     'Usuario': r'^[a-zA-Z-]{3,25}$',
-#     # 'IP_NAS_AP': r'^192\.168\.247\.[0-9]{2}$',
-#     'Inicio_de_Conexión_Dia': r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$',
-#     'Inicio_de_Conexión_Hora': r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$',
-#     'FIN_de_Conexión_Dia': r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$',
-#     'FIN_de_Conexión_Hora': r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$',
-#     'Session_Time': r'^\d+(\.\d+)?$'
-#     # 'MAC_AP':  r'^([0-9A-Fa-f]{2}-){5}[0-9A-Fa-f]{2}:HCDD$',
-#     # 'MAC_Cliente': r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$'
-# }
-
-
-
-# # Cargar el archivo CSV como un DataFrame en Pandas utilizando multiprocessing
-# # def cargar_csv(path):
-# #     data=pd.read_csv(path,low_memory=False,usecols=expresiones_regulares.keys())
-# #     return data
-
-# # Ruta y nombre de tu archivo CSV
-# csv_file = './TP5v2/file/export-2019-to-now-v4.csv'
-
-# data=pd.read_csv(csv_file,low_memory=False,usecols=expresiones_regulares.keys())
-
-
-# # data_filter=data[data['ID'].str.contains(r'^\d{6,7}$') &
-# #                 data['ID_Sesion'].str.contains(r'^[A-F0-9]{8}-[A-F0-9]{8}$') &
-# #                 data['Usuario'].str.contains(r'^[a-zA-Z-]{3,25}$') &
-# #                 data['Inicio_de_Conexión_Dia'].str.contains(r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$') &
-# #                 data['Inicio_de_Conexión_Hora'].str.contains(r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$') &
-# #                 data['FIN_de_Conexión_Dia'].str.contains(r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$') &
-# #                 data['FIN_de_Conexión_Hora'].str.contains(r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$') &
-# #                 data['Session_Time'].str.contains(r'^\d+(\.\d+)?$')]
-
-
-# data_filter = data[data['ID'].astype(str).str.contains(r'^\d{6,7}$') &
-#                    data['ID_Sesion'].astype(str).str.contains(r'^[A-F0-9]{8}-[A-F0-9]{8}$') &
-#                    data['Usuario'].astype(str).str.contains(r'^[a-zA-Z-]{3,25}$') &
-#                    data['Inicio_de_Conexión_Dia'].astype(str).str.contains(r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$') &
-#                    data['Inicio_de_Conexión_Hora'].astype(str).str.contains(r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$') &
-#                    data['FIN_de_Conexión_Dia'].astype(str).str.contains(r'^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8]|3[01])$') &
-#                    data['FIN_de_Conexión_Hora'].astype(str).str.contains(r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$') &
-#                    data['Session_Time'].astype(str).str.contains(r'^\d+(\.\d+)?$')]
-
-
-# print(data_filter)
+        print(' Opción incorrecta, volver a ingresar')
 
